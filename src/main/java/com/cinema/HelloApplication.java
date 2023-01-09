66d614639f6486cf3065bb1f057ea1b8a33752b6
JohnnySwayze <johnnyswayze4@gmail.com>
2023-01-09 06:06:27 +0200

    echo "# kursova" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/JohnnySwayze/kursova.git git push -u origin main

--- src/main/java/com/cinema/HelloApplication.java
+++ src/main/java/com/cinema/HelloApplication.java
@@ -4,13 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
 public class HelloApplication extends Application {
-   
+   DatabaseHandler h=new DatabaseHandler();
      
     @Override
     public void start(Stage stage) throws IOException {
@@ -18,6 +19,7 @@ public class HelloApplication extends Application {
             Parent root= FXMLLoader.load(HelloApplication.class.getResource("hello-view.fxml"));
             Scene scene =new Scene(root);
             stage.setScene(scene);
+            stage.getIcons().add((h.download_picture(4)));
             stage.setTitle("OYE");
             stage.setResizable(false);
             stage.centerOnScreen();
